16b393465794c51a4fea63b16957944b559cf5be
Cris-Omar <c-o.hernandez@hotmail.com>
2023-08-12 00:12:15 +0200

    picture and basics for be patient page

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './styles/style.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>We're building for you, be patient 🙃</h1>
+      <img src="/pictures/construction.jpg" alt="Under Construction" width={840} height={1024} />
+    </div>
+  );
+}
+
+export default App;
